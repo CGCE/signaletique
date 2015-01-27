dcb9890d76828f83293f57a6dce1b15c1fef75df
Jérôme Combes <jerome@planningbiblio.fr>
2015-01-27 15:18:38 +0100

    initial commit

---
+++ data.php
@@ -0,0 +1,55 @@
+<?php
+
+$floor0="RDC / 1<sup>st</sup>";
+$floor1="1<sup>er</sup> / 2<sup>nd</sup>";
+$floor2="2<sup>&egrave;me</sup> / 3<sup>rd</sup>";
+$floor3="3<sup>&egrave;me</sup> / 4<sup>th</sup>";
+
+$tab0[]="Columbia Global Centers | Europe";
+$tab0[]="Columbia University Programs";
+$tab0[]="Member Programs";
+
+$tab1[]=array("Director","101",$floor1,"A");
+$tab1[]=array("Administrative Director","104",$floor1,"A");
+$tab1[]=array("Operations Manager","004",$floor0,"A");
+$tab1[]=array("Program Coordinator","102",$floor1,"A");
+$tab1[]=array("Administrative Coordinator","102",$floor1,"A");
+$tab1[]=array("IT Manager","012",$floor0,"E");
+
+$tab2[]=array("Architecture - GSAPP - Paris Atelier","318",$floor3,"E");
+$tab2[]=array("Architecture - The Shape of Two Cities/New York-Paris","318",$floor3,"E");
+$tab2[]=array("Columbia University Undergraduate Programs in Paris","110",$floor1,"C");
+$tab2[]=array("Mailman School for Public Health","115",$floor1,"A");
+$tab2[]=array("Masters in French Studies","312",$floor3,"C");
+$tab2[]=array("Masters in French History and Literature","312",$floor3,"C");
+
+$tab3[]=array("Academic Year Abroad (AYA)","","309",$floor3,"D");
+$tab3[]=array("Arts Arena","","221",$floor2,"D");
+$tab3[]=array("Association of Americans Resident Overseas (AARO)","","116",$floor2,"E");
+$tab3[]=array("Assoc. Fran&ccedil;aise des Femmes Dipl&ocirc;m&eacute;es des Universit&eacute;s","","009",$floor0,"D");
+$tab3[]=array("Bespoke Education","","224",$floor2,"E");
+$tab3[]=array("Dartmouth College","","220",$floor1,"E");
+$tab3[]=array("Ecole des Hautes Etudes de Sant&eacute; Publique (EHESP)","","117",$floor2,"E");
+$tab3[]=array("EUSA - Academic Internship Programs","","006",$floor0,"B");
+$tab3[]=array("Hamilton College","","204",$floor2,"A");
+$tab3[]=array("Hollins College","","301",$floor3,"A");
+$tab3[]=array("Sarah Lawrence College","","218",$floor2,"E");
+$tab3[]=array("Smith College","","209",$floor2,"B");
+$tab3[]=array("Sustainable Development Solutions Network (UNSDSN)","","120",$floor1,"E");
+$tab3[]=array("The American Club of Paris (APC)","","307",$floor1,"B");
+$tab3[]=array("University of Kent at Paris","","211",$floor2,"C");
+$tab3[]=array("Vassar-Wesleyan Program in Paris","","305",$floor3,"A");
+
+$colonne1=<<<EOD
+<table>
+<tr><td class='col00'>
+</td>
+<td class='col1 ligne1'><span class='subtitle'>Bureau<br/>Office</span></td>
+<td class='col2 ligne1'><span class='subtitle'>Etage<br/>Floor</span></td>
+<td class='col3 ligne1'><span class='subtitle'>Escalier<br/>Stairs</span></td>
+</tr>
+</table>
+EOD;
+?>
+
+
---
+++ inc/db.php
@@ -0,0 +1,127 @@
+<?php
+class db
+	{
+	var $host;
+	var $dbname;
+	var $user;
+	var $password;
+	var $conn;
+	var $result;
+	var $nb;
+	var $error=false;
+
+	function db()
+		{
+		$this->host="localhost:3306";
+		$this->dbname="AffichageDyn";
+		$this->user="AffichageDyn";
+		$this->password="2KrGbN3GSPRYnA6E";
+		}
+			
+	function connect()
+		{
+		$this->conn=mysql_connect($this->host,$this->user,$this->password);
+		mysql_select_db($this->dbname,$this->conn);
+		}
+	
+	function query($requete)
+		{
+		$this->connect();
+		$req=mysql_query($requete,$this->conn);
+		
+		if(!$req)
+			{
+			echo "<br/><br/>### ERREUR SQL ###<br><br>$requete<br/><br/>";
+			echo mysql_error();
+			echo "<br/><br/>";
+			$this->error=true;
+			}
+		elseif(strtolower(substr(trim($requete),0,6))=="select")
+			{
+			$this->nb=mysql_num_rows($req);
+			for($i=0;$i<$this->nb;$i++)
+				$this->result[]=mysql_fetch_array($req);
+			}
+		$this->disconnect();
+		}
+
+	function disconnect()
+		{
+		mysql_close($this->conn);
+		}
+
+	function insert2($table,$values){
+		$fields=join(",",array_keys($values));
+		$values=join("','",$values);
+
+		$requete="INSERT INTO `{$GLOBALS['config']['dbprefix']}$table` ($fields) VALUES ('$values');";
+		$this->query($requete);
+	}
+
+	function select($table,$where=1,$items="*"){
+		$requete="SELECT $items FROM `{$GLOBALS['config']['dbprefix']}$table` WHERE $where;";
+		$this->query($requete);
+	}
+
+	function update($table,$set,$where)
+		{
+		$requete="UPDATE `{$GLOBALS['config']['dbprefix']}$table` SET $set WHERE $where";
+		$this->query($requete);
+		}
+
+	function update2($table,$fields,$ids){
+		$tab=array();
+		$keys=array_keys($fields);
+		foreach($keys as $elem){
+			  $tab[]="$elem='{$fields[$elem]}'";
+  		}
+		$set=join(",",$tab);
+
+		$tab=array();
+		$keys=array_keys($ids);
+		foreach($keys as $elem){
+			  $tab[]="$elem='{$ids[$elem]}'";
+ 		}
+		$where=join(",",$tab);
+
+		$this->update($table,$set,$where);
+	}
+	}
+
+
+class dbh{
+  var $dbhost;
+  var $dbname;
+  var $dbuser;
+  var $dbpass;
+  var $pdo;
+  var $stmt;
+  var $result;
+
+
+  function dbh(){
+	    $this->dbhost="localhost";
+	    $this->dbname="events";
+	    $this->dbuser="events";
+	    $this->dbpass="2KrGbN3GSPRYnA6E";
+
+	    $this->pdo=new PDO("mysql:host={$this->dbhost};dbname={$this->dbname}",$this->dbuser,$this->dbpass);
+	    }
+
+  function exec($sql){
+	    $this->pdo->exec($sql);
+	    }
+
+  function prepare($sql){
+	    $this->stmt=$this->pdo->prepare($sql);
+	    }
+
+  function execute($data){
+	    $this->stmt->execute($data);
+	    $this->result=$this->stmt->fetch();
+	    }
+
+}
+
+
+?>
